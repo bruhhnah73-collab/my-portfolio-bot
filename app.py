463147951de0dd15d07d279cf1ed2098f4951987
bruhhnah73-collab <bruhhnah73@gmail.com>
2026-09-02 18:20:54 +0545

    Update app.py

--- app.py
+++ app.py
@@ -45,12 +45,12 @@ if prompt := st.chat_input("Ask me about projects..."):
         for msg in st.session_state.messages:
             api_messages.append({"role": msg["role"], "content": msg["content"]})
             
-                    # ✅ Change it to this exactly:
         completion = client.chat.completions.create(
             model="llama-3.3-70b", # <-- ACTIVE PRODUCTION ENGINE ID!
             messages=api_messages,
             stream=True
         )
+
 
 
         for chunk in completion:
